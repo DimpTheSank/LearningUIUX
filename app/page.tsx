136f525048288648d0363d7a09bdf19244833497
DimpTheSank <dangnlh.auto@gmail.com>
2026-05-19 23:41:18 +0700

    check deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-<div>
-  <h1>Chào mừng bạn!</h1>
-  <p>Đây là trang web đầu tiên của tôi.</p>
-  <button>Xem thêm</button>
-</div>
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Chào mừng bạn!</h1>
+      <p>Đây là trang web đầu tiên của tôi.</p>
+      <button>Xem thêm</button>
+    </div>
+  );
+}
